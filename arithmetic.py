8d0abbab8db84e0e0e77f6db64cbaefae29c9327
Nayangupta-55 <nayan111155@gmail.com>
2025-04-04 12:49:33 +0530

    Practice

--- arithmetic.py
+++ arithmetic.py
@@ -43,15 +43,40 @@
 # print("Rs.200 off on bill : ", sum-200)
 # print("20% off : ", sum - sum*20/100)
 
-divNo =15
-print("Table of ",divNo)
-print(divNo , "X 1 =", divNo * 1)
-print(divNo , "X 2 =", divNo * 2)
-print(divNo , "X 3 =", divNo * 3)
-print(divNo , "X 4 =", divNo * 4)
-print(divNo , "X 5 =", divNo * 5)
-print(divNo , "X 6 =", divNo * 6)
-print(divNo , "X 7 =", divNo * 7)
-print(divNo , "X 8 =", divNo * 8)
-print(divNo , "X 9 =", divNo * 9)
-print(divNo , "X 10 =", divNo * 10)
+# divNo =15
+# print("Table of ",divNo)
+# print(divNo , "X 1 =", divNo * 1)
+# print(divNo , "X 2 =", divNo * 2)
+# print(divNo , "X 3 =", divNo * 3)
+# print(divNo , "X 4 =", divNo * 4)
+# print(divNo , "X 5 =", divNo * 5)
+# print(divNo , "X 6 =", divNo * 6)
+# print(divNo , "X 7 =", divNo * 7)
+# print(divNo , "X 8 =", divNo * 8)
+# print(divNo , "X 9 =", divNo * 9)
+# print(divNo , "X 10 =", divNo * 10)
+
+#Floor division it gives result in integer it roundoff the result in nearest integer
+#print(13//2)
+
+#WHOLESALE PACKET
+# Chips = 366.6868
+# Wafers = 656.89876
+# Biscuits = 5575.98978
+# Kurkure = 6566.78686
+# Red = 76657.9897787
+# Yellow= 7567.89886
+# Green= 9090.7656
+# Sum = Chips + Wafers + Biscuits + Kurkure + Red + Yellow + Green
+# print(Sum)
+# print("Bill divided in 8 people",Sum//8)
+
+name = input("What's your name :"),
+age = int(input("What's your age :")),
+qualification = input("Tell me about your educational qualification :"),
+hobby = input("Tell me about your hobbies"),
+job= input("What's your current job position :"),
+company = input("Tell me in which company you work :"),
+salary = int(input("What's your salary package :")),
+work = int(input("How many hour you work :"))
+print(name, age, qualification , hobby, job , company , salary , work )
