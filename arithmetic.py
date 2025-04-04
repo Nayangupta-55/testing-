2316d3b75c29d8bc78335e9eaa29eb76798a53a9
Nayangupta-55 <nayan111155@gmail.com>
2025-04-04 11:52:10 +0530

    Dyanamic Table

--- arithmetic.py
+++ arithmetic.py
@@ -1,9 +1,57 @@
-dinner = 2000
-coffee = 400
-softDrink = 440
-burger = 200
-fries = 40
-sum = dinner + coffee + softDrink + burger + fries
-print("Total Bill",sum)
-print("4% Total Bill",sum/4)
-print(sum/4*4)
+# dinner = 2000
+# coffee = 400
+# softDrink = 440
+# burger = 200
+# fries = 40
+# sum = dinner + coffee + softDrink + burger + fries
+# print("Total Bill",sum)
+# print("4% Total Bill",sum/4)
+# print(sum/4*4)
+
+#DAY SCHEDULE
+# Breakfast = 100
+# Travel = 100
+# Lunch = 258
+# Projectwork = 547
+# Snacks = 58
+# Dinner = 300
+# Sweets = 50
+# College = 1700
+# sum = Breakfast + Travel+Lunch+Projectwork+Snacks+Dinner+Sweets+College 
+
+# print("sum value : " ,sum )
+# print("Bill split between 6 people : ", sum/6)
+# print("Rs.200 off on bill : ", sum-200)
+# print("20% off : ", sum - sum*20/100)
+
+
+
+#USER INPUT
+
+# Breakfast = int(input("Enter the breakfast value:"))
+# Travel = int(input("Enter the travel value:"))
+# Lunch = int(input("Enter the lunch value:"))
+# Projectwork = int(input("Enter the projectwork value:"))
+# Snacks = int(input("Enter the snacks value:"))
+# Dinner = int(input("Enter the dinner value:"))
+# Sweets = int(input("Enter the sweets value:"))
+# College = int(input("Enter the college value:"))
+# sum = Breakfast + Travel+Lunch+Projectwork+Snacks+Dinner+Sweets+College 
+
+# print("sum value : " ,sum )
+# print("Bill split between 6 people : ", sum/6)
+# print("Rs.200 off on bill : ", sum-200)
+# print("20% off : ", sum - sum*20/100)
+
+divNo =15
+print("Table of ",divNo)
+print(divNo , "X 1 =", divNo * 1)
+print(divNo , "X 2 =", divNo * 2)
+print(divNo , "X 3 =", divNo * 3)
+print(divNo , "X 4 =", divNo * 4)
+print(divNo , "X 5 =", divNo * 5)
+print(divNo , "X 6 =", divNo * 6)
+print(divNo , "X 7 =", divNo * 7)
+print(divNo , "X 8 =", divNo * 8)
+print(divNo , "X 9 =", divNo * 9)
+print(divNo , "X 10 =", divNo * 10)
