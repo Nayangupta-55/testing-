723304c66cff24c03bcbea4b14fbd4c5b935a55e
Nayangupta-55 <nayan111155@gmail.com>
2025-04-04 11:02:01 +0530

    arith

---
+++ file.py
@@ -0,0 +1,29 @@
+print("my name is nayan gupta")
+print('my','name','is','nayan','gupta',sep=("!"))
+
+#How to user input a number value , the number is always integer must be and we using the in function
+# age = int(input("Enter your age: "))
+# print("You are", age, "years old.")
+
+
+# age = float(input("Enter your age: "))
+# print("You are", age, "years old.")
+
+# print("my\n name\n is\n nayan\n gupta") #NEW LINE (\n)
+# print("my\t name\t is\t nayan\t gupta") #TAB (\t)
+
+name = input("Enter your name: ")
+print(name)
+
+account = int(input("Enter your account no.: "))
+print("Your account no. is",account)
+
+ifsc = int(input("Enter your IFSC code: "))
+print("Your IFSC code is",ifsc)
+ 
+branch = input("Enter your branch address: ") 
+print(branch)
+
+address = input("Enter your address: ")
+print(address)
+
